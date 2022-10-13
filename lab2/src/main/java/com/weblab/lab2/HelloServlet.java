0e9bb09bc80cac6dd08bbf85d567f8e35f99c64a
AlexZ2Z <pletnev.sasa@gmail.com>
2022-10-13 12:19:35 +0300

    hello-commit

--- lab2/src/main/java/com/weblab/lab2/HelloServlet.java
+++ lab2/src/main/java/com/weblab/lab2/HelloServlet.java
@@ -1,18 +1,17 @@
 package com.weblab.lab2;
 
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            message = "Hello World!";
         response.setContentType("text/html");
 
         // Hello
@@ -20,8 +19,5 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
-    }
-
-    public void destroy() {
     }
 }
